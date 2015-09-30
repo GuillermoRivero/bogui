61a48dda348cebae71000cb331ab6e28a087347f
Guillermo Rivero Rodríguez <guillermotegueste@gmail.com>
2015-09-30 15:39:11 +0100

    uploaded old bogui files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BORIddsdASO' });
+  res.render('index', { title: 'BoguiJS' });
 });
 
 module.exports = router;
