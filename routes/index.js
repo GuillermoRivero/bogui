593307155546d93fbf8f7dce38adc48db38aac58
Guillermo Rivero Rodríguez <guillermotegueste@gmail.com>
2015-10-05 13:19:14 +0100

    added enviroment variable

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var enviroment = process.env.NODE_ENV || "develop"
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BoguiJS' });
+  if(enviroment === 'staging'){
+    res.render('error', { message: 'staging' });
+
+  }else{
+    res.render('index', { title: 'BoguiJS' });
+  }
 });
 
 module.exports = router;
